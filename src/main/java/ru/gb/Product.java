8e5b12f30bff96269f2949226a9d2c0d2360f379
LakhmatovSergei <svlakhmatov@gmail.com>
2022-12-30 23:54:46 +0300

    With Product section.

---
+++ src/main/java/ru/gb/Product.java
@@ -0,0 +1,31 @@
+package ru.gb;
+
+
+import lombok.Data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Integer price;
+
+    public Product() {
+    }
+
+    public Product(String title, Integer price) {
+        this.title = title;
+        this.price = price;
+    }
+}
---
+++ src/main/java/ru/gb/ProductDao.java
@@ -0,0 +1,11 @@
+package ru.gb;
+
+import java.util.List;
+
+public interface ProductDao {
+
+    Product findById(Long id);
+    List<Product> findAll();
+    void deleteById(Long id);
+    Product saveOrUpdate(Product product);
+}
